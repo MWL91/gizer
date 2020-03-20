877c8035785744e5d016b02bdf40f21d6afe1913
Marcin Lenkowski <m.lenkowski@asper.net.pl>
2020-03-20 15:24:38 +0100

    Code coverage at 89.58% by removing unused files + add test for fetch on get

--- tests/Integration/ResultsControllerTest.php
+++ tests/Integration/ResultsControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Integration;
 
 use Tests\TestCase;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 
 class ResultsControllerTest extends TestCase
 {
@@ -32,6 +33,23 @@ class ResultsControllerTest extends TestCase
 
     public function test_can_get_data_from_api_ordered_desc_by_finished_at()
     {
+        $response = $this->json('get', 'api/results', [
+            'order' => 'desc',
+            'order_by' => 'finished_at',
+        ]);
+
+        $decodedResponse = $response->decodeResponseJson();
+        $firstScore = $decodedResponse[0];
+        $lastScore = end($decodedResponse);
+
+        $response->assertStatus(200);
+        $this->assertTrue($firstScore['finished_at'] >= $lastScore['finished_at']);
+    }
+
+    public function test_can_get_data_from_api_with_fetch()
+    {
+        Config::set('api.fetch.always', true);
+
         $response = $this->json('get', 'api/results', [
             'order' => 'desc',
             'order_by' => 'finished_at',
